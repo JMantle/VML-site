fdc735b97a1de2b8d679d946c1d55ac8663d5bbe
josh <josh@penguin>
2025-05-07 19:21:04 +0100

    updated readme and annotated code more consistently

--- README.md
+++ README.md
@@ -25,3 +25,7 @@ team management tools
 # Permission
 
 please only view this code and share it for viewing. Please contact vailminorleaguevml@gmail.com for enquiries.
+
+# Note on Design
+
+Most of the HTML and CSS used in this project was generated with the assistance of AI tools or was made by someone else and implemented with their permission. My primary focus was on backend development, authentication, and data handling. Design elements were integrated to support the functionality and improve user experience.
--- app.py
+++ app.py
@@ -19,40 +19,42 @@ def get_db_connection():
 # handle attempted log in
 @app.route("/loginSubmit", methods=["POST"])
 def inputSubmitted():
+    #get data
     username = request.form["username"]
     attemptedPassword = request.form["password"]
+    #search to see if account exists
     conn = get_db_connection()
     found = conn.execute("SELECT password FROM logins WHERE username = ?", (username,)).fetchone()
     conn.close()
     if found:
+        #check password against one stored
         if check_password_hash(found[0], attemptedPassword):
+            #set session variables
             session["loggedIn"] = True
             session["username"] = username
+            #give user permissions if they need them
             (captain, admin) = checkPerms(username)
-            print(captain + admin)
             session["captain"] = captain
             session["adminperms"] = admin
+            #take user to index page
             return redirect("/index")
         else:
+            #incorrect password
             flash("wrong password")
     else:
+        #account does not exists
         flash("no accounts with this username")
+    #allow user to try again on same page
     return redirect("/loginPage")
 
 # check account perms
 def checkPerms(username):
+    #get users permissions from database
     conn = get_db_connection()
     (captain, admin) = conn.execute("SELECT captain, admin FROM logins WHERE username = ?", (username,)).fetchone()
-    if captain == 1:
-        isCaptain = True
-    else:
-        isCaptain = False
-    if admin == 1:
-        isAdmin = True
-    else:
-        isAdmin = False
     conn.close()
-    return isCaptain, isAdmin
+    #cast them to bool
+    return bool(captain), bool(admin)
 
 
 # send user to sign up page
@@ -63,12 +65,13 @@ def goToSignUp():
 # handle attempted sign up
 @app.route("/signUpSubmit", methods=["POST"])
 def signUp():
+    #get username and check it is valid (no commas)
     username = request.form["username"]
     if ',' in username:
         flash('Username cannot contain commas.')
         return redirect('/signUpPage')
     else:
-        hashedPassword = generate_password_hash(request.form["password"])
+        #make sure username noot taken
         conn = get_db_connection()
         found = conn.execute("SELECT password FROM logins WHERE username = ?", (username,)).fetchone()
         if found:
@@ -76,9 +79,12 @@ def signUp():
            flash("Username already taken")
            return render_template("/signUpPage.html")
         else:
+            #hash password and put all data in database
+            hashedPassword = generate_password_hash(request.form["password"])
             conn.execute("INSERT INTO logins (username, password) VALUES (?, ?)", (username, hashedPassword))
             conn.commit()
             conn.close()
+            #let user log in
             return redirect("/loginPage")
     
 # send user to log in page
@@ -88,6 +94,7 @@ def loginPage():
 
 # STANDINGS
 
+#fetch standings and put them in place order
 def getStandings():
     conn = get_db_connection()
     teams = conn.execute("SELECT * FROM teams ORDER BY place ASC").fetchall()
@@ -96,10 +103,12 @@ def getStandings():
 
 # GAMES
 
+#get upcoming game data 
 def getUpcomingGames(team):
     now = datetime.now()
     now_str = now.strftime("%Y-%m-%d %H:%M:%S")
     conn = get_db_connection()
+    #detect if it needs to be a specific team
     if len(team) > 0:
         games = conn.execute("SELECT * FROM games WHERE datetime > ? AND (home = ? OR away = ?)", (now_str, team, team)).fetchall()
     else:
@@ -107,29 +116,39 @@ def getUpcomingGames(team):
     conn.close()
     return games
 
+#localise the time specific to the user
 def utc_to_local(utc_dt, timezone_str):
     local_tz = pytz.timezone(timezone_str)
     local_dt = utc_dt.replace(tzinfo=pytz.utc).astimezone(local_tz)
     return local_dt
 
+#get games and change the times to the user's timezone
 def getTimezonedGames(team):
+    #get times and timezone
     games_data = getUpcomingGames(team)
     user_timezone_str = session.get('timezone') 
 
     updated_games = []
 
+    #localize each game
     for game in games_data:
+        #get utc datetime
         game_dict = dict(game) 
         utc_dt = datetime.strptime(game_dict['datetime'], '%Y-%m-%d %H:%M:%S')
 
+        #localize it if able to get user's timezone
         if user_timezone_str:
+            #give game local datetie
             game_dict['local_datetime'] = utc_to_local(utc_dt, user_timezone_str).strftime('%Y-%m-%d %H:%M:%S')
+            # set abbreviated timezone
             session["abbreviatedTimezone"] = utc_dt.astimezone(pytz.timezone(user_timezone_str)).tzname()
 
         else:
+            #give user UTC times if not able to get timezone
             game_dict['local_datetime'] = game_dict['datetime']
             session["abbreviatedTimezone"] = "UTC"
 
+        #put games back together again
         updated_games.append(game_dict)
 
     return updated_games
@@ -137,18 +156,26 @@ def getTimezonedGames(team):
 
 # TEAM
 
+#show all team data on specific team
 @app.route("/showTeam/<string:teamName>")
 def team(teamName):
+    #get team stats
     conn = get_db_connection()
     stats = conn.execute("SELECT * FROM teams WHERE name = ?", (teamName,)).fetchone()
+    #get upcoming games for that team
     games = getTimezonedGames(teamName)
+    #get membership requests for captain if the user is the captain
     if session.get("loggedIn") and stats["captain"] == session["username"]:
         requests = conn.execute("SELECT * FROM requests WHERE teamid = ?", (stats["id"],)).fetchall()
         conn.close()
         return render_template("team.html", stats=stats, games=games, captain=True, member=True, requests=requests)
     else:
+        #take team members
         members = stats["members"] if stats["members"] is not None else ""
-        if session["username"] + ", " in members or bool(conn.execute("SELECT 1 FROM requests WHERE username = ? AND teamid = ?", (session["username"], stats["id"]))):
+        #get if the user has already requested membership 
+        requested = bool(conn.execute("SELECT 1 FROM requests WHERE username = ? AND teamid = ?", (session["username"], stats["id"])))
+        #decide whether to show the request membership button or not.
+        if (session["loggedIn"] and session["username"] in members) or  requested:
             member = True
         else:
             member = False
@@ -156,9 +183,12 @@ def team(teamName):
 
         return render_template("team.html", stats=stats, games=games, captain=False, member=member)
     
+#handle user requesting membership
 @app.route("/requestMembership/<int:teamId>/<string:username>", methods=["POST"])
 def requestMembership(teamId, username):
+    #get message
     message = request.form.get("message")
+    #put it in database
     conn = get_db_connection()
     conn.execute("INSERT INTO requests (teamid, username, message) VALUES (?, ?, ?)", (teamId, username, message))
     conn.commit()
@@ -171,13 +201,16 @@ def requestMembership(teamId, username):
 
 # MANAGE TEAM
 
+#handle manage team being clicked
 @app.route("/manageTeam")
 def manageTeam():
+    #find team to redirect to
     conn = get_db_connection()
     team = conn.execute("SELECT name FROM teams WHERE captain = ?", (session["username"], )).fetchone()
     conn.close()
     return redirect("/showTeam/" + team[0])
 
+#handle removing member from team
 @app.route("/removeFromTeam/<int:id>/<string:member>", methods=["POST"])
 def removeFromTeam(id, member):
     #get members
@@ -185,6 +218,8 @@ def removeFromTeam(id, member):
     members = conn.execute("SELECT members FROM teams WHERE id = ?", (id,)).fetchone()
 
     #remove sepcific member
+
+    #place each member in a list
     membersString = members[0]
     members = []
     while len(membersString) > 0:
@@ -195,7 +230,10 @@ def removeFromTeam(id, member):
         else:
             members.append(membersString[:comma])
             membersString = membersString[comma + 2:]
+    #remove the chosen member
     members.remove(member)
+
+    #put back into string to store
     membersString = ""
 
     #add all items except last with comma
@@ -212,28 +250,39 @@ def removeFromTeam(id, member):
 
     return redirect("/manageTeam")
 
+#handle adding members
 @app.route("/addMember/<int:id>", methods=["POST"])
 def addMember(id):
+    #get name
     name = request.form.get("name").strip()
 
+    #get current members
     conn = get_db_connection()
     members = conn.execute("SELECT members FROM teams WHERE id = ?", (id,)).fetchone()[0]
 
+    #add to string
     if members:
+        #add to end if members already there
         members += ", " + name
     else:
+        #become the first member
         members = name
 
+    #update database
     conn.execute("UPDATE teams SET members = ? WHERE id = ?", (members, id))
     conn.commit()
     conn.close()
+    #return to managing team
     return redirect("/manageTeam")
 
+#handle assigning players to games
 @app.route("/assignPlayers/<int:gameId>/<string:teamName>", methods=["POST"])
 def assignPlayers(gameId, teamName):
+    #get data
     players = request.form.get("players").strip()
     conn = get_db_connection()
 
+    #figure out whether team is home or away
     side = conn.execute("SELECT home FROM games WHERE id = ?", (gameId,)).fetchone()
     if side[0] == teamName:
         #home team
@@ -245,8 +294,10 @@ def assignPlayers(gameId, teamName):
     conn.commit()
     conn.close()
 
+    #return to manage team
     return redirect("/manageTeam")
 
+#handle accepting membership requests
 @app.route("/acceptRequest/<int:id>", methods=["POST"])
 def acceptRequest(id):
     conn = get_db_connection()
@@ -263,13 +314,16 @@ def acceptRequest(id):
 
     return redirect("/manageTeam")
 
+#handle declining request
 @app.route("/declineRequest/<int:id>", methods=["POST"])
 def declineRequest(id):
+    #delete it
     deleteRequest(id)
     return redirect("/manageTeam")
 
 # ADMIN
 
+#handle going to admin page
 @app.route("/admin")
 def admin():
     #get data
@@ -285,21 +339,25 @@ def admin():
 
 # users
 
+#get all users
 def getUsers():
     conn = get_db_connection()
     users = conn.execute("SELECT * FROM logins").fetchall()
     conn.close()
     return users
 
+#handle updating user permissions
 @app.route("/updateUser/<string:id>", methods=["POST"])
 def updateUser(id):
     conn = get_db_connection()
 
+    #invert user's captaincy
     if "makeCaptain" in request.form:
         row = conn.execute("SELECT captain FROM logins WHERE id = ?", (id,)).fetchone()
         current = row["captain"]
         conn.execute("UPDATE logins SET captain = ? WHERE id = ?", (0 if current else 1, id))
 
+    #invert user's admin permission
     elif "makeAdmin" in request.form:
         row = conn.execute("SELECT admin FROM logins WHERE id = ?", (id,)).fetchone()
         current = row["admin"]
@@ -311,10 +369,12 @@ def updateUser(id):
 
 # teams
 
+#handle updating team data
 @app.route("/updateTeam/<int:id>", methods=["POST"])
 def updateTeam(id):
     conn = get_db_connection()
 
+    #get inputted data
     name = request.form.get("name").strip()
     mapWins = request.form.get("mapwins").strip()
     matchWins = request.form.get("matchwins").strip()
@@ -323,8 +383,10 @@ def updateTeam(id):
     points = request.form.get("points").strip()
     mmr = request.form.get("mmr").strip()
 
+    #get previous data
     team = conn.execute("SELECT * FROM teams WHERE id = ?", (id,)).fetchone()
 
+    #update data if inputted, otherwise result to previous data
     updatedName = name if name else team['name']
     updatedMapWins = mapWins if mapWins else team['mapwins']
     updatedMatchWins = matchWins if matchWins else team['matchwins']
@@ -333,18 +395,22 @@ def updateTeam(id):
     updatedPoints = points if points else team['points']
     updatedmmr = mmr if mmr else team["mmr"]
 
+    #return to database
     conn.execute("UPDATE teams SET name = ?, mapwins = ?, matchwins = ?, captain = ?, members = ?, points = ?, mmr = ? WHERE id = ?", (updatedName, updatedMapWins, updatedMatchWins, updatedCaptain, updatedMembers, updatedPoints, updatedmmr, id))
 
     conn.commit()
     conn.close()
 
+    #update their places (mmr could be changed affecting the order of the teams)
     sortTeams()
 
     return redirect("/admin")
 
+#handle creation of teams
 @app.route("/createTeam", methods=["POST"])
 def createTeam():
 
+    #get data
     name = request.form.get("name").strip()
     mapwins = request.form.get("mapwins").strip()
     matchwins = request.form.get("matchwins").strip()
@@ -352,19 +418,28 @@ def createTeam():
     points = request.form.get("points").strip()
     mmr = request.form.get("mmr").strip()
 
+    #make team using the data
     makeTeam(name, mapwins, matchwins, captain, points, mmr)
+    
+    #update teams places
     sortTeams()
+
     return redirect("/admin")
 
+#handle deleting team
 @app.route("/deleteTeam/<int:id>", methods=["POST"])
 def deleteTeamRoute(id):
+    #delete team
     deleteTeam(id)
     return redirect("/admin")
 
+#update the teams' places
 def sortTeams():
+    #get teams and their mmr
     conn = get_db_connection()
     teams = conn.execute("SELECT id, mmr FROM teams").fetchall()
 
+    #put them in a list (named array because i like to think of it as an array)
     array = []
     for i in range(0,len(teams)):
         array.append(teams[i])
@@ -384,14 +459,16 @@ def sortTeams():
     conn.commit()
     conn.close()
 
-    #procedure
+    #procedure so not returning anything
             
 # games
 
+#handle updating game
 @app.route("/updateGame/<int:id>", methods=["POST"])
 def updateGame(id):
     conn = get_db_connection()
 
+    #get data
     home = request.form.get("home").strip()
     away = request.form.get("away").strip()
     datetime = request.form.get("datetime").strip()
@@ -399,8 +476,10 @@ def updateGame(id):
     awayPlayers = request.form.get("awayplayers").strip()
     other = request.form.get("other").strip()
     
+    #get previous data
     game = conn.execute("SELECT * FROM games WHERE id = ?", (id,)).fetchone()
 
+    #update data if there is new data, otherwise use previous data
     updatedHome = home if home else game['home']
     updatedAway = away if away else game['away']
     updatedDatetime = datetime if datetime else game['datetime']
@@ -408,6 +487,7 @@ def updateGame(id):
     updatedAwayPlayers = awayPlayers if awayPlayers else game["awayplayers"]
     updatedOther = other if other else game['other']
 
+    #put it back in database
     conn.execute("UPDATE games SET home = ?, away = ?, datetime = ?, homeplayers = ?, awayplayers = ?, other = ? WHERE id = ?", (updatedHome, updatedAway, updatedDatetime, updatedHomePlayers, updatedAwayPlayers, updatedOther, id))
 
     conn.commit()
@@ -415,24 +495,34 @@ def updateGame(id):
 
     return redirect("/admin")
 
+#handle deleting game
 @app.route("/deleteGame/<int:id>", methods=["POST"])
 def deleteGameRoute(id):
+    #delete game
     deleteGame(id)
     return redirect("/admin")
 
+#handle creating a game
 @app.route("/createGame", methods=["POST"])
 def createGame():
 
+    #get data
     home = request.form.get("home").strip()
     away = request.form.get("away").strip()
     datetime = request.form.get("datetime").strip()
 
+    #make game
     makeGame(home, away, datetime)
+
+    #update places
     sortTeams()
+    
     return redirect("/admin")
 
+#handle deleting messages
 @app.route("/deleteMessage/<int:id>", methods=["POST"])
 def deleteMessageRoute(id):
+    #delete message
     deleteMessage(id)
     return redirect("/admin")
 
@@ -441,10 +531,13 @@ def deleteMessageRoute(id):
 
 # INDEX
 
+#handle home page
 @app.route("/index")
 def index():
+    #get data
     teams = getStandings()
     games = getTimezonedGames("")
+
     return render_template("index.html", teams=teams, games=games)
 
 #contact
@@ -502,7 +595,7 @@ def root():
 
     return render_template("load.html")
 
-
+#prevent accidental running
 if __name__ == "__main__":
     app.run(debug=True)
 
